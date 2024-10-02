909705c69d5afe64817ca6d99fc42f4a2f09da78
Gihan Ediriweera Jayakuru <gihan22603@gmail.com>
2024-10-02 15:36:39 +0530

    seeders

--- database/seeders/CustomersTableSeeder.php
+++ database/seeders/CustomersTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Customer;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,13 @@ class CustomersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Customer::create([
+            'name' => 'Gihan',
+            'company' => 'GEJ pvt.',
+            'phone' => '0772520310',
+            'email' => 'gihan@example.com',
+            'country' => 'SL',
+            'status' => 'Active',
+        ]);
     }
 }
--- database/seeders/ProjectsTableSeeder.php
+++ database/seeders/ProjectsTableSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Customer;
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +12,26 @@ class ProjectsTableSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+       
+        $project1 = Project::create([
+            'name' => 'Project Alpha',
+            'description' => 'This is a description for Project Alpha.'
+        ]);
+
+        $project2 = Project::create([
+            'name' => 'Project Beta',
+            'description' => 'This is a description for Project Beta.'
+        ]);
+
+        
+        $customerIds = Customer::pluck('id')->all();
+
+        // assign customers to projects,
+        if (!empty($customerIds)) {
+            $project1->customers()->attach($customerIds);  
+            $project2->customers()->attach(array_slice($customerIds, 0, 1));  
+        }
     }
 }
